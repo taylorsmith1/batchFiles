0b2063f88f9dbdc6af2d9d2b351959d49fd79206
Taylor Smith <taylornsmith1000@gmail.com>
2019-02-06 22:40:26 -0500

    Push file created

--- push.bat
+++ push.bat
@@ -7,12 +7,9 @@ set /p x="What would you like to call your commit: "
 echo.
 
 git add %y%
-echo Adding...
 echo.
 
 git commit -m "%x%"
-echo Comitting...
 echo.
 
 git push
-echo "Push Successful!"
