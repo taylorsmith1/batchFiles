f087c712edcbf7836d2f060b2761b406e145816b
Taylor Smith <taylornsmith1000@gmail.com>
2019-02-06 22:44:44 -0500

    Push file updated

--- push.bat
+++ push.bat
@@ -5,6 +5,9 @@ set /p y="What would you like to commit (* for all files): "
 echo.
 set /p x="What would you like to call your commit: "
 echo.
+set /p z="Are you sure you want to push (y or n): "
+
+IF %z% == n DO (/exit)
 
 git add %y%
 echo.
